7616bc2b5bc55a190894321043cae812954b313b
dawin.kweon <dawin.kweon@eroad.com>
2023-08-19 18:51:12 +1200

    fix: update to allow prod build

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 "use client";
-import React, { useEffect } from 'react';
-import Post from './Post';
-import CreatePost from './CreatePost';
-import styles from './page.module.css'
-import { useRouter } from "next/navigation"
-import { signOut } from 'firebase/auth';
-import { auth } from '../../firebaseConfig'
-import Link from 'next/link';
+import React, { useEffect, useState } from "react";
+import Post from "./Post";
+import CreatePost from "./CreatePost";
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebaseConfig";
+import Link from "next/link";
 export default function Home() {
   const logout = async () => {
     console.log("Logging out...");
@@ -15,17 +15,28 @@ export default function Home() {
     router.push("/login");
   };
 
-  const isLoggedin = auth.currentUser != null;
-  console.log("User is logged in? " + isLoggedin);
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
-  const router = useRouter()
-  if (!isLoggedin) {
-    router.push("/login");
+  useEffect(() => {
+    const isLoggedin = auth.currentUser != null;
+    if (!isLoggedin) {
+      router.push("/login");
+    } else {
+      setIsUserLoggedIn(true);
+    }
+  }, []);
+  console.log("User is logged in? " + isUserLoggedIn);
+
+  const router = useRouter();
+  if (!isUserLoggedIn) {
+    return <span>hi</span>;
   } else {
     return (
       <div className={styles.container}>
         <h1>Green Kiwi</h1>
-        <button type="button" onClick={logout}>sign out</button>
+        <button type="button" onClick={logout}>
+          sign out
+        </button>
         <CreatePost />
         <Post />
         <Post />
@@ -37,31 +48,51 @@ export default function Home() {
           <ul>
             <li>
               <div className={styles.logo}>
-                <img src="/eco-house.png" alt="eco house logo" style={{ width: '30px', height: '30', marginLeft: 4 }} />
+                <img
+                  src="/eco-house.png"
+                  alt="eco house logo"
+                  style={{ width: "30px", height: "30", marginLeft: 4 }}
+                />
               </div>
               <Link href="/">Home</Link>
             </li>
             <li>
               <div className={styles.logo}>
-                <img src="/user.png" alt="user logo" style={{ width: '30px', height: '30', marginLeft: 15 }} />
+                <img
+                  src="/user.png"
+                  alt="user logo"
+                  style={{ width: "30px", height: "30", marginLeft: 15 }}
+                />
               </div>
               <Link href="/dashboard">Dashboard</Link>
             </li>
             <li>
               <div className={styles.logo}>
-                <img src="/layout.png" alt="layout logo" style={{ width: '30px', height: '30', marginLeft: 7 }} />
+                <img
+                  src="/layout.png"
+                  alt="layout logo"
+                  style={{ width: "30px", height: "30", marginLeft: 7 }}
+                />
               </div>
               <Link href="/freebies">Freebies</Link>
             </li>
             <li>
               <div className={styles.logo}>
-                <img src="/learning.png" alt="learning logo" style={{ width: '30px', height: '30', marginLeft: 8 }} />
+                <img
+                  src="/learning.png"
+                  alt="learning logo"
+                  style={{ width: "30px", height: "30", marginLeft: 8 }}
+                />
               </div>
               <Link href="/learning">Learning</Link>
             </li>
             <li>
               <div className={styles.logo}>
-                <img src="/present.png" alt="present logo" style={{ width: '30px', height: '30', marginLeft: 3 }} />
+                <img
+                  src="/present.png"
+                  alt="present logo"
+                  style={{ width: "30px", height: "30", marginLeft: 3 }}
+                />
               </div>
               <Link href="/reward">Reward</Link>
             </li>

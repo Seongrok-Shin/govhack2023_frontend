a01fdc5218a15152971ef8edae5534d617dfb1c5
jinwoo park <elvintern@gmail.com>
2023-08-19 18:54:18 +1200

    Modify styling

--- src/app/CreatePost.jsx
+++ src/app/CreatePost.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import './CreatePost.scss'
+import React from 'react';
+import './CreatePost.scss';
 
 export default function CreatePost() {
   return (
     <div className="create-post">
-        <div className="post__container">
-          <img src="https://randomuser.me/api/portraits/men/1.jpg" className="profile__picture"></img>
-          <p className="post__name">What's on your mind?</p>
-        </div>
-          <button className='post__button post__button--'>
-          <i class="bi bi-plus-circle post__icon"></i>
-          </button>
+      <div className="post__container">
+        <img
+          src="https://randomuser.me/api/portraits/men/1.jpg"
+          className="profile__picture"
+        ></img>
+        <p className="create-post__text">What's on your mind?</p>
       </div>
-  )
+      <button className="post__button post__button--post">
+        <i class="bi bi-plus-circle post__icon"></i>
+      </button>
+    </div>
+  );
 }
--- src/app/Post.jsx
+++ src/app/Post.jsx
@@ -1,40 +1,47 @@
-'use client'
+'use client';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './Post.scss';
 
 function Post() {
   return (
-    <Card className='post'>
+    <Card className="post">
       <div className="profile">
         <div className="profile__container">
-          <img src="https://randomuser.me/api/portraits/men/1.jpg" className="profile__picture"></img>
+          <img
+            src="https://randomuser.me/api/portraits/men/1.jpg"
+            className="profile__picture"
+          ></img>
           <p className="profile__name">John Doe</p>
         </div>
         <div className="profile__buttons">
-          <button className='profile__button'>
-          <i class="bi bi-pencil profile__icon"></i>
+          <button className="profile__button">
+            <i class="bi bi-pencil profile__icon"></i>
           </button>
-          <button className='profile__button'>
-          <i class="bi bi-trash3 profile__icon"></i>
+          <button className="profile__button">
+            <i class="bi bi-trash3 profile__icon"></i>
           </button>
         </div>
       </div>
       <Card.Body>
-       <Card.Text>
+        <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Card.Img className='profile__image' variant="top" src="https://randomuser.me/api/portraits/men/1.jpg" />
+        <Card.Img
+          className="profile__image"
+          variant="top"
+          src="https://randomuser.me/api/portraits/men/1.jpg"
+        />
         <div className="profile__buttons profile__buttons--like">
-          <div className='likes__container'>
+          <div className="likes__container">
             <i class="bi bi-hearts profile__icon profile__icon--heart"></i>
-            <button className='likes'>50</button>
+            <button className="likes">50</button>
           </div>
 
-          <div className='likes__container'>
+          <div className="likes__container">
             <i class="bi bi-suit-heart-fill profile__icon profile__icon--heart"></i>
-            <button className='likes'>Like</button>
+            <button className="likes">Like</button>
           </div>
         </div>
       </Card.Body>
